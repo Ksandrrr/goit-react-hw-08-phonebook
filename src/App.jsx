59de055bf5ef9296cfc0510cdee055f8670b15a8
Ksandrrr <105207003+Ksandrrr@users.noreply.github.com>
2023-03-14 13:19:01 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { store, persistor } from "./redux/store";
 import { Navigation } from "./Navigation/Navigation";
 import { PersistGate } from 'redux-persist/integration/react'
 import AuthLayout from "./components/AuthLayout/AuthLayout"
+
 const App = () => {
     return (
     <Provider store={store}>
